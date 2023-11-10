c0e17eb6d8482d11897ec7f654ce91497134a5d8
AndrePerezIPB <148081475+AndrePerezIPB@users.noreply.github.com>
2023-11-10 10:44:38 +0000

    2.Flocking Part 2
    Creation of the flocking rules

--- Assets/Flock.cs
+++ Assets/Flock.cs
@@ -14,6 +14,54 @@ public class Flock : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        ApplyRules();
         this.transform.Translate(0, 0, speed * Time.deltaTime);
+    }
+
+    void ApplyRules()
+    {
+        GameObject[] gos;
+        gos = FlockManager.FM.allFish;
+
+        Vector3 vCenter = Vector3.zero;
+        Vector3 vAvoid = Vector3.zero;
+        float gSpeed = 0.01f;
+        float nDistance;
+        int groupSize = 0;
+        
+        foreach(GameObject go in gos)
+        {
+            if(go != this.gameObject)
+            {
+                nDistance = Vector3.Distance(go.transform.position, this.transform.position);
+                if(nDistance <= FlockManager.FM.neighbourDistance)
+                {
+                    vCenter += go.transform.position;
+                    groupSize++;
+
+                    if(nDistance < 1.0f)
+                    {
+                        vAvoid = vAvoid + (this.transform.position - go.transform.position);
+                    }
+
+                    Flock anotherFlock = go.GetComponent<Flock>();
+                    gSpeed = gSpeed + anotherFlock.speed;
+                }
+            }
+        }
+
+        if(groupSize > 0)
+        {
+            vCenter = vCenter / groupSize;
+            speed = gSpeed / groupSize;
+
+            Vector3 direction = (vCenter + vAvoid) - transform.position;
+            if(direction != Vector3.zero)
+            {
+                transform.rotation = Quaternion.Slerp(transform.rotation,
+                                                        Quaternion.LookRotation(direction),
+                                                        FlockManager.FM.rotationSpeed * Time.deltaTime);
+            }
+        }
     }
 }
